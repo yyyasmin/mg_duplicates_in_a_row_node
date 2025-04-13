14d8173cdbc1baf6dc03760553174a3acf2c5067
unknown <you@example.com>
2025-04-13 19:17:20 +0300

    server url

--- helpers/ServerRoutes.js
+++ helpers/ServerRoutes.js
@@ -1,10 +1,10 @@
 const PROXY_URL = "http://localhost:5000";
-export const RENDER_PROXY_URL = "https://gameName-production.up.railway.app";
+export const RENDER_PROXY_URL = "https://yyyasminallmg-production.up.railway.app/";
 
 /// DUMMY TO FORCE PUSH
 
-const CHOSEN_PROXY_URL = PROXY_URL;
-//const CHOSEN_PROXY_URL = RENDER_PROXY_URL;
+//const CHOSEN_PROXY_URL = PROXY_URL;
+const CHOSEN_PROXY_URL = RENDER_PROXY_URL;
 
 module.exports = {
   PROXY_URL,
