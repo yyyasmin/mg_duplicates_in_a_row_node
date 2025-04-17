d416882a38bce88514cdb10c9f0793ecfbd840bc
unknown <you@example.com>
2025-04-17 17:13:55 +0300

    server node and flask url

--- helpers/ServerRoutes.js
+++ helpers/ServerRoutes.js
@@ -7,11 +7,13 @@ const NODE_PROXY_URL = "http://localhost:5000";
 const FLASK_PROXY_URL = "http://127.0.0.1:8000";
 
 // For production, use Railway or any hosted URL
-export const NODE_RENDER_URL = "https://yyyasminallmg-production.up.railway.app"; // NODE on Railway
-export const FLASK_RENDER_URL = "https://flask_auth-production.up.railway.app";    // Flask on Railway
+export const NODE_RENDER_URL = "https://mgduplicatesinarownode-production.up.railway.app/"; // NODE on Railway
+
+export const FLASK_RENDER_URL = "https://mgduplicatrsinarowflask-production.up.railway.app/";    // Flask on Railway
 
 // Switch between local and render here
-const USE_LOCAL = true;
+//const USE_LOCAL = true;
+const USE_LOCAL = false;
 
 // Export chosen URLs for use in frontend
 export const CHOSEN_NODE_URL = USE_LOCAL ? NODE_PROXY_URL : NODE_RENDER_URL;
