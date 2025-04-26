12f8d4255cd3cf7be2e7c26c07fc55429ca34eda
unknown <you@example.com>
2025-04-26 21:41:39 +0300

    BROWSER URL

--- src/helpers/ServerRoutes.js
+++ src/helpers/ServerRoutes.js
@@ -18,7 +18,7 @@ export const NODE_RENDER_URL = "https://mgduplicatesinarownode-production.up.rai
 
 export const FLASK_RENDER_URL = "https://mgduplicatrsinarowflask-production.up.railway.app";    // Flask on Railway
 
-export const BROWSER_RENDER_URL = "https://mgduplicatrsinarowflask-production.up.railway.app";    // FOR ROOMS SQUARE IMAGE PRDEIX 
+export const BROWSER_RENDER_URL = NODE_RENDER_URL
 
 
 ////// CHOSE ENVIROMENT VARS //////
